98ee04ade0d63c48bad5aef392cd6e00336a8bfd
David Cardona <david_ec10@hotmail.com>
2019-07-22 00:37:16 -0500

    Agregar archivos de proyecto.

---
+++ BibliotecaDominio/Bibliotecario.cs
@@ -0,0 +1,129 @@
+﻿using BibliotecaDominio.IRepositorio;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BibliotecaDominio
+{
+    public class Bibliotecario
+    {
+        public const string EL_LIBRO_NO_SE_ENCUENTRA_DISPONIBLE = "El libro no se encuentra disponible";
+        private  IRepositorioLibro libroRepositorio;
+        private  IRepositorioPrestamo prestamoRepositorio;
+
+
+        public Bibliotecario(IRepositorioLibro libroRepositorio, IRepositorioPrestamo prestamoRepositorio)
+        {
+            this.libroRepositorio = libroRepositorio;
+            this.prestamoRepositorio = prestamoRepositorio;
+        }
+
+        public void Prestar(string isbn, string nombreUsuario)
+        {
+            //throw new Exception("se debe implementar este método");
+            int cantDiasPrestamo = 0;
+            if (palindromo(isbn) == true)
+            {
+                throw new Exception("Los libros palíndromos solo se pueden utilizar en la biblioteca");
+            }
+            else
+            {
+                if (EsPrestado(isbn) == true)
+                {
+                    throw new Exception(EL_LIBRO_NO_SE_ENCUENTRA_DISPONIBLE);
+                }
+                else
+                {
+                    DateTime fechaSolicitud = DateTime.Now;
+                    DateTime fechaEntregaMaxima = new DateTime();
+                    if (SumaNumerosISBN(isbn) >= 30)
+                    {
+                        cantDiasPrestamo = 15;
+                        fechaEntregaMaxima = CalcularFechaEntregaMaxima(fechaSolicitud, cantDiasPrestamo);
+                    }
+                    else
+                    {
+                        cantDiasPrestamo = 16;
+                        fechaEntregaMaxima = CalcularFechaEntregaMaxima(fechaSolicitud, cantDiasPrestamo);
+                    }             
+                    libroRepositorio.ObtenerPorIsbn(isbn);
+                    Prestamo oPrestamo = new Prestamo(fechaSolicitud, libroRepositorio.ObtenerPorIsbn(isbn), fechaEntregaMaxima, nombreUsuario);
+                    prestamoRepositorio.Agregar(oPrestamo);
+                }
+            }
+        }
+
+
+        public bool EsPrestado(string isbn)
+        {
+            try
+            {
+                prestamoRepositorio.ObtenerLibroPrestadoPorIsbn(isbn);
+                string isbnPrestado = prestamoRepositorio.ObtenerLibroPrestadoPorIsbn(isbn).Isbn;
+                if (isbnPrestado == isbn)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public bool palindromo(string texto)
+        {
+            String textoInv = "", caracter;
+            int textoTamano = texto.Length;
+            for (int i = textoTamano - 1; i >= 0; i--)
+            {
+                caracter = texto.Substring(i, 1);
+                textoInv = textoInv + caracter;
+            }
+            if (texto == textoInv)
+                return true;
+            else
+                return false;
+        }
+
+
+        public int SumaNumerosISBN(string texto)
+        {
+            char caracter;
+            int textoTamano = texto.Length, acumNumeros=0, caracterNumerico=0;
+            for (int i = textoTamano - 1; i >= 0; i--)
+            {
+                caracter = Convert.ToChar(texto.Substring(i, 1));
+                if (char.IsNumber(caracter))
+                {
+                    caracterNumerico = Convert.ToInt32(caracter.ToString());
+                    acumNumeros = acumNumeros + caracterNumerico;
+                }
+            }
+            return acumNumeros;
+        }
+
+        public DateTime CalcularFechaEntregaMaxima(DateTime fechaSolicitud, int cantDias)
+        {
+            DateTime fechaEntregaMaxima = new DateTime();
+            for (int i = 0; i < cantDias-1; i++)
+            {
+                if (fechaSolicitud.DayOfWeek.ToString() == "Saturday")
+                {
+                    fechaEntregaMaxima = fechaSolicitud.AddDays(2);
+                }
+                else
+                {
+                    fechaEntregaMaxima = fechaSolicitud.AddDays(1);
+                }
+                fechaSolicitud = fechaEntregaMaxima;
+            }
+            return fechaEntregaMaxima;
+        }
+    }
+}
---
+++ DominioTest/Unitarias/BibliotecarioTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BibliotecaDominio;
+using BibliotecaDominio.IRepositorio;
+using DominioTest.TestDataBuilders;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace DominioTest.Unitarias
+{
+    [TestClass]
+    public class BibliotecarioTest
+    {
+        public BibliotecarioTest()
+        {
+
+        }
+        public Mock<IRepositorioLibro> repositorioLibro;
+        public Mock<IRepositorioPrestamo> repositorioPrestamo;
+
+        [TestInitialize]
+        public void setup()
+        {
+            repositorioLibro = new Mock<IRepositorioLibro>();
+           repositorioPrestamo = new Mock<IRepositorioPrestamo>();
+        }
+
+        [TestMethod]
+        public void EsPrestado()
+        {
+            // Arrange
+            var libroTestDataBuilder = new LibroTestDataBuilder();
+            Libro libro = libroTestDataBuilder.Build();
+            
+            repositorioPrestamo.Setup(r => r.ObtenerLibroPrestadoPorIsbn(libro.Isbn)).Returns(libro);
+
+            // Act
+            Bibliotecario bibliotecario = new Bibliotecario(repositorioLibro.Object,repositorioPrestamo.Object);
+            var esprestado = bibliotecario.EsPrestado(libro.Isbn);
+
+            // Assert
+            Assert.AreEqual(esprestado, true);
+        }
+
+        [TestMethod]
+        public void LibroNoPrestadoTest()
+        {
+            // Arrange
+            var libroTestDataBuilder = new LibroTestDataBuilder();
+            Libro libro = libroTestDataBuilder.Build();
+            Bibliotecario bibliotecario = new Bibliotecario(repositorioLibro.Object, repositorioPrestamo.Object);
+            repositorioPrestamo.Setup(r => r.ObtenerLibroPrestadoPorIsbn(libro.Isbn)).Equals(null);
+
+            // Act
+            var esprestado = bibliotecario.EsPrestado(libro.Isbn);
+
+            // Assert
+            Assert.IsFalse(esprestado);
+        }
+
+        [TestMethod]
+        public void LibroPalindromo()
+        {
+            // Arrange
+            string isbnTest = "A12X21A";
+            Bibliotecario bibliotecario = new Bibliotecario(repositorioLibro.Object, repositorioPrestamo.Object);
+
+            // Act
+            var palindromo = bibliotecario.palindromo(isbnTest);
+
+            // Assert
+            Assert.AreEqual(palindromo, true);
+        }
+
+        [TestMethod]
+        public void SumarNumerosISBN()
+        {
+            // Arrange
+            string isbnTest = "A12X21A";
+            Bibliotecario bibliotecario = new Bibliotecario(repositorioLibro.Object, repositorioPrestamo.Object);
+
+            // Act
+            var sumaNumerosISBN = bibliotecario.SumaNumerosISBN(isbnTest);
+
+            // Assert
+            Assert.AreEqual(sumaNumerosISBN, 6);
+        }
+
+
+
+    }
+}
